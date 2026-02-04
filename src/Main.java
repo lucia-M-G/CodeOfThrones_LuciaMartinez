1933605e553d95e903d2fa009d1ece19b2ce89ac
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2026-02-04 13:39:20 +0100

    feat: pasar parámetros a los constructores

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,12 @@ public class Main {
     }
 
     public static void inicio() {
-        Guerrero heroe = new Guerrero();
-        Mago nigromante = new Mago();
-        Monstruo ogro = new Monstruo();
+        Guerrero heroe = new Guerrero("Melinoë", 10, 100, 15);
+        Mago nigromante = new Mago("Grigori Rasputín", 8, 60, 50);
+        Monstruo licantropo = new Monstruo("Remus Lupin", 5, 80);
+
+        heroe.atacar();
+        nigromante.atacar();
+        licantropo.atacar();
     }
 }
