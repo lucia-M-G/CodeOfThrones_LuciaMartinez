1b52325fbb29c6cba2a4181990badaa2b17287d1
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2026-02-06 13:40:25 +0100

    feat: crear enum para tipos de daño

---
+++ src/Personaje/TipoDanio.java
@@ -0,0 +1,9 @@
+package Personaje;
+
+public enum TipoDanio {
+    FISICO,      //Espadas - golpes
+    MAGICO,      //Hechizos
+    VENENO,      //Veneno
+    FUEGO,       //Fuego
+    HIELO        //Hielo
+}
